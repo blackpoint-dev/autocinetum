b36572524ca8827432741eefcc621acc70c7eab3
acerblaze <edward.baltaza@gmail.com>
2024-05-12 16:31:06 +0200

    Offer's carousel bug fixes

--- app/page.js
+++ app/page.js
@@ -1,113 +1,408 @@
+// pages/index.js
+"use client";
+import React, { Component } from "react";
+import { useEffect, useState } from "react";
+import { fetchEntries } from "../lib/contentful";
 import Image from "next/image";
+import Link from "next/link";
+import Slider from "react-slick";
+
+const ArrowStyles = {
+  display: "block",
+  background: "transparent",
+  border: "none",
+  zIndex: 1,
+};
+
+// Custom Previous Arrow
+const PrevArrow = ({ className, style, onClick }) => {
+  return (
+    <>
+      <style jsx>{`
+        .custom-slick-prev,
+        .custom-slick-next {
+          background: transparent !important;
+          border: none !important;
+          height: 32px;
+          width: 32px;
+        }
+        .custom-slick-prev:before,
+        .custom-slick-next:before {
+          display: none;
+        }
+      `}</style>
+      <div
+        className={`custom-slick-prev ${className}`}
+        style={{
+          ...style,
+          display: "block",
+          zIndex: 1,
+          left: "-40px",
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+        onClick={onClick}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path
+            fill="#000000"
+            d="m8.82 12l7.715 7.715q.22.222.218.53t-.224.528t-.529.221t-.529-.22L7.83 13.136q-.242-.243-.354-.54T7.363 12t.112-.596t.354-.54L15.47 3.22q.221-.221.532-.218q.31.003.532.224t.22.529t-.22.529z"
+          />
+        </svg>
+      </div>
+    </>
+  );
+};
+
+// Custom Next Arrow
+const NextArrow = ({ className, style, onClick }) => {
+  return (
+    <>
+      <style jsx>{`
+        .custom-slick-prev,
+        .custom-slick-next {
+          background: transparent !important;
+          border: none !important;
+          height: 32px;
+          width: 32px;
+        }
+        .custom-slick-prev:before,
+        .custom-slick-next:before {
+          display: none;
+        }
+      `}</style>
+      <div
+        className={`custom-slick-next ${className}`}
+        style={{
+          ...style,
+          display: "block",
+          zIndex: 1,
+          right: "-40px",
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+        onClick={onClick}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          transform="rotate(180)"
+        >
+          <path
+            fill="#000000"
+            d="m8.82 12l7.715 7.715q.22.222.218.53t-.224.528t-.529.221t-.529-.22L7.83 13.136q-.242-.243-.354-.54T7.363 12t.112-.596t.354-.54L15.47 3.22q.221-.221.532-.218q.31.003.532.224t.22.529t-.22.529z"
+          />
+        </svg>
+      </div>
+    </>
+  );
+};
 
 export default function Home() {
+  const [carListing, setCarListing] = useState([]);
+
+  useEffect(() => {
+    async function getEntries() {
+      const allEntries = await fetchEntries();
+      setCarListing(allEntries);
+    }
+
+    getEntries();
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    lazyLoad: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    rows: 2,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
+    <div className="flex flex-col justify-center space-y-24">
+      <img
+        className="w-full"
+        srcSet="/about/banner-1-about-sm.png 768w, /about/banner-1-about.jpg 1920w"
+        sizes="(max-width: 767px) 100vw, 1920px"
+        src="/banner-1-about.jpg"
+        alt="Banner"
+        width="1920"
+        height="275"
+      />
+      <div className="space-y-6">
+        <p className="text-2xl font-bold text-center">Dobrý den,</p>
+        <p className="text-lg text-center">
+          jsme prodejce prověřených vozů, kterým věnujeme maximální péči.
+          <br />
+          Najdete u nás především kvalitní vozidla s českým původem a
+          pravidelným servisem.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+      <div className="mx-4 md:mx-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        <div className="flex flex-col text-center space-y-4">
+          <Image
+            className="self-center mt-3"
+            src="/about/about-1.svg"
+            width={80}
+            height={80}
+            alt="icon"
+          />
+          <p className="text-xl font-bold">Oblíbené vozy</p>
+          <p className="text-lg">
+            nabízíme prodej a výkup nejen oblíbených koncernových vozů
+          </p>
+        </div>
+        <div className="flex flex-col text-center space-y-4">
+          <Image
+            className="self-center"
+            src="/about/about-2.svg"
+            width={80}
+            height={80}
+            alt="icon"
+          />
+          <p className="text-xl font-bold">Ověřená historie</p>
+          <p className="text-lg">
+            vozidla zakoupená jako nová v ČR se snadno dohledatelnou historií
+          </p>
+        </div>
+        <div className="flex flex-col text-center space-y-4">
+          <Image
+            className="self-center"
+            src="/about/about-3.svg"
+            width={80}
+            height={80}
+            alt="icon"
+          />
+          <p className="text-xl font-bold">Možnost financování</p>
+          <p className="text-lg">
+            zajistíme vám výhodné financování i pojištění
+          </p>
+        </div>
+        <div className="flex flex-col text-center space-y-4">
+          <Image
+            className="self-center"
+            src="/about/about-4.svg"
+            width={80}
+            height={80}
+            alt="icon"
+          />
+          <p className="text-xl font-bold">Ověření CEBIA</p>
+          <p className="text-lg">
+            všechna naše vozidla prošla kontrolou v databázi od společnosti
+            CEBIA
+          </p>
+        </div>
+        <div className="flex flex-col text-center space-y-4">
+          <Image
+            className="self-center"
+            src="/about/about-5.svg"
+            width={80}
+            height={80}
+            alt="icon"
+          />
+          <p className="text-xl font-bold">Pohodový výběr</p>
+          <p className="text-lg">
+            osobní přístup, profesionální komunikace a vystupování
+          </p>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="relative w-full">
+        <img
+          className="w-full"
+          srcSet="/about/banner-2-about-sm.png 768w, /about/banner-2-about.jpg 1920w"
+          sizes="(max-width: 767px) 100vw, 1920px"
+          src="/about/banner-2-about.jpg"
+          alt="Banner"
+          width="1920"
+          height="150"
+        />
+        <div className="absolute top-12 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <Image
+            priority
+            src="/about/car.png"
+            width={306.5}
+            height={157.4}
+            alt="Car"
+          />
+        </div>
+      </div>
+      <div className="text-brown mx-4">
+        <p className="text-2xl font-bold text-center">Aktuální nabídka</p>
+        <div className="w-full mx-auto px-2 md:px-8 lg:px-32 mt-12">
+          <Slider {...settings}>
+            {carListing.map(({ fields }) => (
+              <div className="px-3 my-3" key={fields.modelName}>
+                <Link
+                  href="/offer"
+                  passHref
+                  className="block overflow-hidden shadow-sm transform transition duration-200 ease-in-out hover:scale-105"
+                >
+                  <div className="relative w-full h-44 overflow-hidden">
+                    {fields.mainPhoto && fields.mainPhoto.fields.file.url && (
+                      <Image
+                        className="object-cover"
+                        objectPosition="center"
+                        layout="fill"
+                        src={`https:${fields.mainPhoto.fields.file.url}`}
+                        alt="Car photo"
+                      />
+                    )}
+                  </div>
+                  <div className="bg-gradient-to-r from-green-500 to-blue-500 h-0.5 w-full"></div>
+                  <div className="bg-light p-3 space-y-2 text-sm">
+                    <h2 className="text-lg font-bold">{fields.modelName}</h2>
+                    <p>{fields.description}</p>
+                    <div>
+                      <p>{fields.price} KČ</p>
+                      <div className="flex flex-row justify-between">
+                        <p>
+                          {fields.vat
+                            ? "možnost odpočtu DPH"
+                            : "ne možnost odpočtu DPH"}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+      <div>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
           priority
+          src="/about/testimonials-1-about.jpg"
+          width={1920}
+          height={260}
+          alt="Car"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <div className="space-y-16">
+        <div>
+          <p className="text-2xl font-bold text-center">Naše další služby</p>
+          <p className="text-lg text-center">
+            Není to pro nás jen o prodeji auta...
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
+        <div className="mx-4 md:mx-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="flex flex-col items-center text-center space-y-10">
+            <div className="relative w-full flex justify-center">
+              <div className="relative">
+                <Image
+                  src="/circle-bg.svg"
+                  width={120}
+                  height={120}
+                  alt="icon"
+                />
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <p className="text-xl font-bold z-10 text-white">
+                    Výkup vozu
+                  </p>
+                </div>
+              </div>
+            </div>
+            <p className="text-lg">
+              Vykoupíme váš vůz, případně individuálně zajistíme zprostředkování
+              prodeje jinou formou.
+            </p>
+          </div>
+          <div className="flex flex-col items-center text-center space-y-10">
+            <div className="relative w-full flex justify-center">
+              <div className="relative">
+                <Image
+                  src="/circle-bg.svg"
+                  width={120}
+                  height={120}
+                  alt="icon"
+                />
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <p className="text-xl font-bold z-10 text-white">
+                    Kontrola vozu
+                  </p>
+                </div>
+              </div>
+            </div>
+            <p className="text-lg">
+              Máme dlouholeté zkušenosti s výkupem vozů, a proto vám rádi
+              pomůžeme s výběrem a fyzickou kontrolou vámi vyhlídnutého auta
+              mimo naši nabídku.
+            </p>
+          </div>
+          <div className="flex flex-col items-center text-center space-y-10">
+            <div className="relative w-full flex justify-center">
+              <div className="relative">
+                <Image
+                  src="/circle-bg.svg"
+                  width={120}
+                  height={120}
+                  alt="icon"
+                />
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <p className="text-xl font-bold z-10 text-white">
+                    Rozleštění laku
+                  </p>
+                </div>
+              </div>
+            </div>
+            <p className="text-lg">
+              Navrátíme lesk vašemu vozu s použitím profesionálních přípravků a
+              provedeme i drobné retuše pro zakrytí nedokonalostí.
+            </p>
+          </div>
+          <div className="flex flex-col items-center text-center space-y-10">
+            <div className="relative w-full flex justify-center">
+              <div className="relative">
+                <Image
+                  src="/circle-bg.svg"
+                  width={120}
+                  height={120}
+                  alt="icon"
+                />
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <p className="text-xl font-bold z-10 text-white">
+                    Čištění interiéru
+                  </p>
+                </div>
+              </div>
+            </div>
+            <p className="text-lg">
+              Provádíme precizní čištění interiéru auta, tepování čalounění a
+              ošetření plastových částí.
+            </p>
+          </div>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
